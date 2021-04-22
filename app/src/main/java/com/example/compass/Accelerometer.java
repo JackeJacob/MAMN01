70aca7ceae04b8c9e64e8df533d45e272a50390f
JackeJacob <ja0032kr-s@student.lu.se>
2021-04-22 19:33:56 +0200

    fixed compass and added accelerometer screen + values

--- app/src/main/java/com/example/compass/Accelerometer.java
+++ app/src/main/java/com/example/compass/Accelerometer.java
@@ -1,6 +1,42 @@
 package com.example.compass;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Accelerometer extends AppCompatActivity {
+public class Accelerometer extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    float x, y, z;
+    TextView acceleration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(Accelerometer.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        acceleration = findViewById(R.id.accelerate);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        x = sensorEvent.values[0];
+        y = sensorEvent.values[1];
+        z = sensorEvent.values[2];
+        acceleration.setText("X: " + x +"\nY: " + y + "\nZ: " + z);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
--- app/src/main/java/com/example/compass/Compass.java
+++ app/src/main/java/com/example/compass/Compass.java
@@ -24,26 +24,29 @@ public class Compass extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private ImageView compassimage;
     private float DegreeStart = 0f;
-    TextView DegreeValues;
-    Sensor accSensor, magnetSensor, vector;
-    float[] gravity;
-    float[] geomagnetic;
+    private TextView DegreeValues;
+    private Sensor accSensor, magnetSensor, vector;
+    boolean havesensor= false, havesensor2=false;
     private int azimuth = 0;
-    private float[] accelerometer = new float[3];
-    private float[] magnetometer = new float[3];
+    float[] lastAccelerometer = new float[3];
+    float[] lastMagnetometer = new float[3];
+    private boolean lastAccelerometerSet;
+    private boolean lastMagnetometerSet;
+    float rMat[] = new float[9];
+    float orientation[] = new float[9];
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
-        compassimage = (ImageView) findViewById(R.id.compass);
-        DegreeValues = (TextView) findViewById(R.id.values);
+        compassimage =  findViewById(R.id.compass);
+        DegreeValues =  findViewById(R.id.values);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        vector = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
     }
 
@@ -58,16 +61,35 @@ public class Compass extends AppCompatActivity implements SensorEventListener {
         super.onResume();
         sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_GAME);
         sensorManager.registerListener(this, magnetSensor, SensorManager.SENSOR_DELAY_GAME);
-        sensorManager.registerListener(this, vector, SensorManager.SENSOR_DELAY_GAME);
 
     }
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float degree = Math.round(event.values[0]);
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
 
-        DegreeValues.setText("Heading towards " + Float.toString(degree) + "°");
+        if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD){
+            System.arraycopy(event.values, 0, lastMagnetometer,0, event.values.length);
+            lastMagnetometerSet = true;
+        }
+
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            System.arraycopy(event.values,0,lastAccelerometer,0,event.values.length);
+            lastAccelerometerSet = true;
+        }
+
+        if(lastAccelerometerSet && lastMagnetometerSet){
+            SensorManager.getRotationMatrix(rMat,null,lastAccelerometer,lastMagnetometer);
+            SensorManager.getOrientation(rMat,orientation);
+            azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+       azimuth = Math.round(azimuth);
+
+        DegreeValues.setText("Heading towards " + Float.toString(azimuth) + "°");
 
         RotateAnimation rotation = new RotateAnimation(DegreeStart, -azimuth, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotation.setFillAfter(true);
